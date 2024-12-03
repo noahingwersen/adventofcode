28c0391d8833b754f31fc02045c4280cf1f2c62a
Noah Ingwersen <noah.ingwersen1@gmail.com>
2024-12-03 19:54:29 +0000

    Modify setup script

--- 2024/setup.sh
+++ 2024/setup.sh
@@ -22,11 +22,11 @@ touch $gofile
 
 # File template
 printf "package main\n\n" >> $gofile
-printf "import (\n\t\"bufio\"\n\t\"os\"\n)\n\n" >> $gofile
+printf "import (\n\t\"bufio\"\n\t\"fmt\"\n\t\"os\"\n)\n\n" >> $gofile
 printf "func check(e error) {\n\tif e != nil {\n\t\tpanic(e)\n\t}\n}\n\n" >> $gofile
 printf "const filepath = \"test.txt\"\n\n" >> $gofile
 printf "func parseInput() {\n\tfile, ferr := os.Open(filepath)\n\tcheck(ferr)\n\n\tscanner := bufio.NewScanner(file)\n\t" >> $gofile
 printf "for scanner.Scan() {\n\t\tline := scanner.Text()\n\n\t}\n\tfile.Close()\n}\n\n" >> $gofile
 printf "func part1() int {\n\treturn 0\n}\n\n" >> $gofile
 printf "func part2() int {\n\treturn 0\n}\n\n" >> $gofile
-printf "func main() {\n\n}\n" >> $gofile
+printf "func main() {\n\tfmt.Println(part1())\n\tfmt.Println(part2())\n}\n" >> $gofile
