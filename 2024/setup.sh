3a527797e29704dff90a2d29aa8a335418bb1080
Noah Ingwersen <noah.ingwersen1@gmail.com>
2024-12-26 21:18:51 +0000

    Update setup script

--- 2024/setup.sh
+++ 2024/setup.sh
@@ -22,11 +22,10 @@ touch $gofile
 
 # File template
 printf "package main\n\n" >> $gofile
-printf "import (\n\t\"bufio\"\n\t\"fmt\"\n\t\"os\"\n\t\"time\"\n)\n\n" >> $gofile
+printf "import (\n\t\"bytes\"\n\t\"fmt\"\n\t\"os\"\n\t\"time\"\n)\n\n" >> $gofile
 printf "const filepath = \"test.txt\"\n\n" >> $gofile
-printf "func parseInput() []string {\n\tfile, _ := os.Open(filepath)\n\n\t" >> $gofile
-printf "scanner := bufio.NewScanner(file)\n\tvar lines []string\n\t" >> $gofile
-printf "for scanner.Scan() {\n\t\tlines = append(lines, scanner.Text())\n\t}\n\tfile.Close()\n\treturn lines\n}\n\n" >> $gofile
+printf "func parseInput() [][]byte {\n\tfile, _ := os.ReadFile(filepath)\n\t" >> $gofile
+printf "return bytes.Split(file, []byte(\"\\\\n\"))\n}\n\n" >> $gofile
 printf "func part1() int {\n\treturn 0\n}\n\n" >> $gofile
 printf "func part2() int {\n\treturn 0\n}\n\n" >> $gofile
 printf "func main() {\n\tstart := time.Now()\n\tfmt.Printf(\"Part 1: %%v in %%v\\\\n\", part1(), time.Since(start))\n\thalf := time.Now()\n\tfmt.Printf(\"Part 2: %%v in %%v\\\\n\", part2(), time.Since(half))\n}" >> $gofile
